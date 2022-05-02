2eb00738599efcb547cd1bc9a35c71f53f1d1833
chloe0b0 <chloezeiter@gmail.com>
2022-05-02 13:53:24 -0700

    Newton's Method

---
+++ src/Newton.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+double f(double x){
+    return pow(x, 2) - 2.0;
+}
+
+double f_(double x){
+    return 2.0*x;
+}
+
+double NewtonsMethod(double (*f)(double), double (*f_)(double), unsigned int max_iters){
+    double x0, x1, y, y_;
+    x0 = 1.0;
+
+    for (int i = 0; i < max_iters; ++i){
+        y = f(x0);
+        y_ = f_(x0);
+
+        x1 = x0 - (y/y_);
+        x0 = x1;
+    }
+
+    return x1;
+}
+
+int main(void){
+    double root = NewtonsMethod(&f, &f_, 50);
+    printf("%f \n", root);
+}
